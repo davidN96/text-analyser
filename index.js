9bac6fb94cbb868c5041ad08196a97c0153930e7
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-02-14 16:24:31 +0100

    preapareLines finished

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ import lineReaderSync from "line-reader-sync";
 
 const argv = minimist(process.argv.slice(2));
 const pathToFile = argv.p || argv.path;
+const lineReader = new lineReaderSync(pathToFile);
 
-const textAnalyze = {
-  spaces: 0,
-  emptyLines: 0,
-  lines: 0,
-  signs: 0
+const prepareLines = () => {
+  const arrayOfLines = lineReader.toLines();
+
+  const trimmedLines = arrayOfLines.map((line, index) => {
+    if (index !== arrayOfLines.length - 1)
+      line = line.substring(0, line.length - 1);
+    return line;
+  });
+
+  return trimmedLines;
 };
+
+const analyze = () => {
+  console.log(trimmedLines);
+};
+analyze();
