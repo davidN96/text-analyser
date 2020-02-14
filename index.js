92bd5e8bb62249840b7a1a5c27ee1644b43bd1f4
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-02-14 15:52:03 +0100

    line-reader added

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import minimist from "minimist";
+import lineReader from "line-reader";
 import colors from "colors";
-import fs from "fs";
-import readline from "readline";
 
 const argv = minimist(process.argv.slice(2));
+const pathToFile = argv.p || argv.path;
 
-console.log(argv);
+const textAnalyze = {
+  spaces: 0,
+  emptyLines: 0,
+  lines: 0,
+  signs: 0
+};
+
+lineReader.eachLine(pathToFile, (line, last) => {
+  textAnalyze.lines++;
+  if (line.length === 0) textAnalyze.emptyLines++;
+
+  for (let i = 0; i < line.length; i++) {
+    if (line.charAt(i) === " ") textAnalyze.spaces++;
+    if (line.charAt(i) !== " ") textAnalyze.signs++;
+  }
+
+  if (last) console.log(textAnalyze);
+});
